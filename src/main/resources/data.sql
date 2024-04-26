1b20d41a09030ce67ed63a51e73c8baf05aa0d11
Thiago Silva <euthiagosrodrigues@gmail.com>
2024-04-26 19:41:50 -0300

    chore: update properties and data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,22 +1,23 @@
 CREATE TABLE person (
     id INTEGER AUTO_INCREMENT NOT NULL,
-    nome VARCHAR(30),
-    email VARCHAR(50),
-    phone VARCHAR(20),
-    cpf VARCHAR(20), 
+    name VARCHAR(30) NULL,
+    email VARCHAR(50) NULL,
+    phone VARCHAR(20) NULL,
+    cpf VARCHAR(20) NULL, 
     created_at datetime NOT NULL,
     updated_at datetime NULL,
     CONSTRAINT PK_person PRIMARY KEY (id),
-    CONSTRAINT UK_personCpf UNIQUE KEY (cpf),
-    CONSTRAINT UK_personEmail UNIQUE KEY (email),
-    CONSTRAINT UK_personPhone UNIQUE KEY (phone)
+    CONSTRAINT UK_personCpf UNIQUE (cpf),
+    CONSTRAINT UK_personEmail UNIQUE  (email),
+    CONSTRAINT UK_personPhone UNIQUE (phone)
 );
 
 CREATE TABLE customer_type (
     id INTEGER AUTO_INCREMENT NOT NULL,
-    nome VARCHAR(30) NOT NULL,
+    name VARCHAR(30) NOT NULL,
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL,
+    is_active BOOLEAN,
     CONSTRAINT PK_customerType PRIMARY KEY (id)
 );
 
@@ -24,13 +25,13 @@ CREATE TABLE customer (
     id INTEGER AUTO_INCREMENT NOT NULL,
     id_person INTEGER NOT NULL,
     id_customer_type INTEGER NOT NULL,
-    payment_day INTEGER,
-    is_active BOOLEAN,
+    payment_day INTEGER NULL,
+    is_active BOOLEAN NOT NULL,
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL,
     CONSTRAINT PK_customer PRIMARY KEY (id),
-    CONSTRAINT FK_person FOREIGN KEY (id_person) REFERENCES person (id),
-    CONSTRAINT FK_customerType FOREIGN KEY (id_customer_type) REFERENCES customer_type (id)
+    CONSTRAINT FK_customerPerson FOREIGN KEY (id_person) REFERENCES person (id),
+    CONSTRAINT FK_customerCustomerType FOREIGN KEY (id_customer_type) REFERENCES customer_type (id)
 );
 
 CREATE TABLE vacancy (
@@ -45,13 +46,14 @@ CREATE TABLE vehicle (
     id INTEGER AUTO_INCREMENT NOT NULL,
     id_customer INTEGER NOT NULL,
     plate VARCHAR(30) NOT NULL,
-    make VARCHAR(30),
+    make VARCHAR(30) NULL,
     model VARCHAR(30) NOT NULL,
     monthly_vehicle BOOLEAN NOT NULL,
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL,
     CONSTRAINT PK_vehicle PRIMARY KEY (id),
-    CONSTRAINT FK_customer FOREIGN KEY (id_customer) REFERENCES vehicle (id)_
+    CONSTRAINT UK_vehiclePlate UNIQUE (plate),
+    CONSTRAINT FK_vehicleCustomer FOREIGN KEY (id_customer) REFERENCES vehicle (id)
 );
 CREATE TABLE ticket (
     id INTEGER AUTO_INCREMENT NOT NULL,
@@ -59,14 +61,14 @@ CREATE TABLE ticket (
     id_vacancy INTEGER NOT NULL,
     id_vehicle INTEGER NOT NULL,
     entry_time DATETIME NOT NULL,
-    exit_time DATETIME,
-    total_price FLOAT,
+    exit_time DATETIME NULL,
+    total_price FLOAT NULL,
     created_at DATETIME NOT NULL,
     updated_at DATETIME NOT NULL,
     CONSTRAINT PK_ticket PRIMARY KEY (id),
-    CONSTRAINT FK_vehicle FOREIGN KEY (id_vehicle) REFERENCES vehicle (id),
-    CONSTRAINT FK_vacancy FOREIGN KEY (id_vacancy) REFERENCES vacancy (id),
-    CONSTRAINT FK_customer FOREIGN KEY (id_customer) REFERENCES customer (id)
+    CONSTRAINT FK_ticketVehicle FOREIGN KEY (id_vehicle) REFERENCES vehicle (id),
+    CONSTRAINT FK_ticketVacancy FOREIGN KEY (id_vacancy) REFERENCES vacancy (id),
+    CONSTRAINT FK_ticketCustomer FOREIGN KEY (id_customer) REFERENCES customer (id)
 );
 
 INSERT INTO vacancy(`id`, `created_at`, `situation`, `updated_at`)
@@ -105,9 +107,9 @@ VALUES(11, current_timestamp(), true,current_timestamp());
 INSERT INTO vacancy(`id`, `created_at`, `situation`, `updated_at`)
 VALUES(12, current_timestamp(), true,current_timestamp());
 
-INSERT INTO customer_type(`id`, `created_at`,`is_active`,`name`,`updated_at`) 
-VALUES(1, current_timestamp(), true, "Rotativo", current_timestamp());
+INSERT INTO customer_type(`id`, `created_at`, `is_active`, `name`, `updated_at`)
+VALUES(1, current_timestamp(), true, 'Rotativo', current_timestamp());
 
-INSERT INTO customer_type(`id`, `created_at`,`is_active`,`name`,`updated_at`) 
-VALUES(2, current_timestamp(), true, "Mensalista", current_timestamp());
+INSERT INTO customer_type(`id`, `created_at`, `is_active`, `name`, `updated_at`)
+VALUES(2, current_timestamp(), true, 'Mensalista', current_timestamp());
 
